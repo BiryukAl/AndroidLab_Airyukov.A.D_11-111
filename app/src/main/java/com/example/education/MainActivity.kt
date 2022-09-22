3b3f419088aee159cd2ef09eefd746e3c41dbad1
BiryukAl <kerzemyak001@gmail.com>
2022-09-22 18:54:20 +0300

    Complite HW1

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -2,15 +2,60 @@ package com.example.education
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.education.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
+
+    val fragmentsContainerId: Int = R.id.main_fragments_container
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
 
-//        wnbdbk
+
+
+    }
+
+    fun addFragment(fragment: Fragment, tag: String, detachCurrent: Boolean = false) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if (detachCurrent) {
+            supportFragmentManager.findFragmentById(fragmentsContainerId)?.let { currentFragment ->
+                transaction.detach(currentFragment)
+            }
+        }
+
+        transaction
+            .add(fragmentsContainerId, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun addWithRemove(fragment: Fragment, tag: String) {
+        val transaction = supportFragmentManager.beginTransaction()
+        supportFragmentManager.findFragmentById(fragmentsContainerId)?.let { currentFragment ->
+            transaction.remove(currentFragment)
+        }
+        transaction
+            .add(fragmentsContainerId, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(fragmentsContainerId, fragment, tag)
+            .commit()
     }
 
 
---
+++ app/src/main/java/com/example/education/fragments/secondPage/SecondPageFragment.kt
@@ -0,0 +1,70 @@
+package com.example.education.fragments.secondPage
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.education.R
+import com.example.education.databinding.FragmentSecondPageBinding
+
+class SecondPageFragment : Fragment(R.layout.fragment_second_page) {
+
+    private val viewBinding: FragmentSecondPageBinding
+            by viewBinding(FragmentSecondPageBinding::bind)
+
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val count : Int? = arguments?.getInt(ARG_COUNTER_VALUE)
+        val countColor : Int? = arguments?.getInt(ARG_COUNTER_COLOR)
+
+
+        if (count == 0){
+            viewBinding.boxCount.setTransitionVisibility(View.INVISIBLE)
+        }else{
+            viewBinding.boxCount.text = "Count: " + count.toString()
+        }
+
+        if (countColor != null) {
+            when(countColor%3){
+                1 -> viewBinding.boxColor.setBackgroundResource(R.color.red)
+                2 -> viewBinding.boxColor.setBackgroundResource(R.color.blue)
+                0 -> viewBinding.boxColor.setBackgroundResource(R.color.green)
+            }
+        }
+
+
+
+    }
+
+
+    companion object {
+
+        const val SECOND_PAGE_FRAGMENT_TAG = "SECOND_PAGE_FRAGMENT_TAG"
+
+        const val ARG_COUNTER_VALUE = "ARG_COUNTER_VALUE"
+
+        const val ARG_COUNTER_COLOR = "ARG_COUNTER_COLOR"
+
+        @JvmStatic
+        fun getInstance(count: Int, countColor: Int): Fragment{
+            val args = Bundle().apply {
+                putInt(ARG_COUNTER_VALUE, count)
+                putInt(ARG_COUNTER_COLOR, countColor)
+            }
+
+            val fragment = SecondPageFragment()
+            fragment.arguments = args
+            return fragment
+        }
+
+
+
+    }
+
+}
